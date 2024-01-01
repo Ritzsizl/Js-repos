f7548a537f3778150535320a4d41743b349203bd
Ritzsizl <ritzraha4u@gmail.com>
2024-01-01 19:12:14 +0530

    Operations

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -1,26 +1,39 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);  //number
-console.log(typeof (score));  //number
+// console.log(typeof score);  //number
+// console.log(typeof (score));  //number
 
-let scoree = "33"
+// let scoree = "33"
 
-console.log(typeof scoree);  //string
-console.log(typeof (scoree));  //string
+// console.log(typeof scoree);  //string
+// console.log(typeof (scoree));  //string
 
-let valueInNumber = Number(scoree)
+// let valueInNumber = Number(scoree)
 
-console.log(typeof valueInNumber);  //number
-console.log(typeof (valueInNumber));  //number
+// console.log(typeof valueInNumber);  //number
+// console.log(typeof (valueInNumber));  //number
 
-//"33" => 33
-// "33abc" => NaN
-// true => 1; false => 0;
+// //"33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0;
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-//1=> true, 0=> false
-//" " => false
-// "Ritam" => true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// //1=> true, 0=> false
+// //" " => false
+// // "Ritam" => true
+
+//******************OPERATIONS******************** 
+let str1 = "hello"
+let str2 = " Ritam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1+2);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(2+2+"1");
