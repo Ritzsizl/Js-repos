21c9c81089a5dabd0cc10fb7ea3a787fb0f13294
Ritzsizl <ritzraha4u@gmail.com>
2024-01-01 18:10:21 +0530

    datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1452
+let accountEmail = "ritzraha4u@gmail.com"
+var accountPassword = "12435"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "hdfbuhf@gmail.com"
+accountPassword = "ghbfh"
+accountCity = "hbuchbsd"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,8 @@
+//alert(3+3) we are using nodejs not browser so it will show error
+let name = "Ritam" //string
+let age = 19  //number
+let isLoggedIn = false  //boolean
+
+console.log(typeof "Ritam") //string
+console.log(typeof "undefined") // undefined
+console.log(typeof "null")  //object
