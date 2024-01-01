c3c9aa4db1c2371023b757af73915dc50f215acf
Ritzsizl <ritzraha4u@gmail.com>
2024-01-01 19:50:23 +0530

    datatype summary

---
+++ 01-basics/datatypes.summary.js
@@ -0,0 +1,19 @@
+//Primitive 
+//7 types: String, Number, Boolean, Null(empty), undefined, Symbol(make any values unique), BigInt(big values)
+ 
+//Reference Type/Non-Primitive
+//Arrays, Objects, functions
+
+//Array
+const heros = ["shaktiman", "naagraj", "doga"]
+
+//Objects
+let myObj = {
+    name: "Ritam",
+    age: 22
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello World")
+}
