dad635219933aba033557ffdba7b81f740aa4dbd
Ritzsizl <ritzraha4u@gmail.com>
2024-01-02 13:06:05 +0530

    functions

---
+++ 03-basics/01_functions.js
@@ -0,0 +1,41 @@
+
+
+// function sayMyName(){
+//     console.log("R");
+//     console.log("I");
+//     console.log("T");
+//     console.log("A");
+//     console.log("M");
+// }
+
+// sayMyName()
+
+// function addTwoNumbers(number1, Number2){
+//     console.log(number1 + Number2);
+// }
+// addTwoNumbers() //NaN
+// addTwoNumbers(3,4) //7
+// addTwoNumbers(3,a)  //3a
+// addTwoNumbers(3, null)  //3 
+
+// const result1 = addTwoNumbers(3,5)
+// console.log("Result: ", result1);  //undefinedd
+
+// function addTwoNumbers(number1, Number2){
+//     console.log(number1 + Number2);
+//     return result
+// }
+
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ", result); //8
+
+function loginUserMessage(username){
+    if(username === undefined){
+console.log("Please enter a username");
+return
+}
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Ritam"));
+console.log(loginUserMessage());
