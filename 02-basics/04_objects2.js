e483b7ddf1b7a899610a2e61a804313b67ba0626
Ritzsizl <ritzraha4u@gmail.com>
2024-01-02 11:33:41 +0530

    objects2

---
+++ 02-basics/04_objects2.js
@@ -0,0 +1,40 @@
+//const tinderUser = new Object()  //singleton
+const tinderUser = {}  //non-singleton
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+console.log(tinderUser) //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "ritam",
+            lastname: "raha"
+        }
+    }
+}
+
+console.log(regularUser.fullname);  //{ userfullname: { firstname: 'ritam', lastname: 'raha' } }
+console.log(regularUser.fullname.userfullname.firstname);  //ritam
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3) //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }   
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIn' ] //array
+
+console.log(Object.values(tinderUser)); //[ '123abc', 'Sammy', false ]
+
+console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// console.log(tinderUser.hasOwnProprty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProprty('isLogged'));
