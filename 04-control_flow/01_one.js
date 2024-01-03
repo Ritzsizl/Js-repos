a85231a831242408e422f1fd1269f65b070208cc
Ritzsizl <ritzraha4u@gmail.com>
2024-01-03 15:50:09 +0530

    control_flow

---
+++ 04-control_flow/01_one.js
@@ -0,0 +1,11 @@
+// if statement
+const isUserloggedIn = true
+const temperature = 41
+if (temperature < 50){
+    console.log("less than 50");
+} else {   
+    console.log("temperature is greater than 50");
+}
+
+const balance = 1000
+ if (balance> 500) console.log("test")
---
+++ 04-control_flow/03_truthy.js
@@ -0,0 +1,18 @@
+////falsy value : false, 0, -0, BigInt 0n, "", null, undefined , NaN
+//truthy values : "0", 'false, " ", [], {}, function (){}
+
+//Nullish Coalescing Operator (??): null, undefined
+
+let val1;
+let val2;
+let val3;
+let val4;
+val1 = 5 ?? 10
+val2 = null ?? 10
+val3 = undefined ?? 15
+val4 = null ?? 10 ?? 20
+
+console.log(val1);//5
+console.log(val2);//10
+console.log(val3);//15
+console.log(val3);//15
